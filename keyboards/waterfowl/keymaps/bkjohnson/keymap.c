821ab512d4fe2199bf51c5a2566c672951af2a27
Brooks Johnson <brooks.johnson@pm.me>
2024-08-24 11:30:15 -0700

    Setup Boot key and enable tap dance
    This isn't the double-tap miryoku implementation because in VIAL you
    [cannot configure tap dance directly][1] as you would in QMK. Vial
    [already defines `tap_dance_actions`][2] and it doesn't seem possible to
    add to or modify that from a keymap easily.
    
    Even though we can't set this up so that it implements miryoke exactly
    out of the box, we can get close and then use the VIAL GUI to configure
    tap dance the way we want with a double tap.
    
    [1]: https://github.com/vial-kb/vial-qmk/issues/579
    [2]: https://github.com/vial-kb/vial-qmk/blob/aac4e02152b131b050d7b6a8cab9a8f4b1ab989a/quantum/vial.c#L478

--- keyboards/waterfowl/keymaps/bkjohnson/keymap.c
+++ keyboards/waterfowl/keymaps/bkjohnson/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          `-----'   `--------------------'            `--------------------'   `-----'
  */
 [_NAV] = LAYOUT(
-	KC_NO,      KC_NO,		KC_NO,		KC_NO,	    KC_NO,		C(KC_Y),C(KC_V),C(KC_C),C(KC_X),	C(KC_Z),
+	QK_BOOT,    KC_NO,		KC_NO,		KC_NO,	    KC_NO,		C(KC_Y),C(KC_V),C(KC_C),C(KC_X),	C(KC_Z),
 	KC_LGUI,	KC_LALT,	KC_LCTL,	KC_LSFT,	KC_NO,		CW_TOGG,KC_LEFT,KC_DOWN,KC_UP,		KC_RIGHT,
 	KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_INS,	KC_HOME,KC_PGUP,KC_PGDN,	KC_END,
 	KC_NO,		KC_NO,      KC_NO,      KC_NO,		KC_NO,	    KC_NO,	KC_ENT,	KC_BSPC,KC_DEL,     KC_NO
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          `-----'   `--------------------'            `--------------------'   `-----'
  */
 [_MOUSE] = LAYOUT(
-	KC_NO,      KC_NO,		KC_NO,		KC_NO,	    KC_NO,		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,
+	QK_BOOT,    KC_NO,		KC_NO,		KC_NO,	    KC_NO,		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,
 	KC_LGUI,	KC_LALT,	KC_LCTL,	KC_LSFT,	KC_NO,		KC_NO,		KC_MS_LEFT, KC_MS_DOWN,	KC_MS_UP,	KC_MS_RIGHT,
 	KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_WH_L,	KC_WH_D,	KC_WH_U,	KC_WH_R,
 	KC_NO,		KC_NO,      KC_NO,      KC_NO,		KC_NO,	    KC_NO,		KC_MS_BTN2,	KC_MS_BTN1,	KC_MS_BTN3, KC_NO
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *          `-----'   `--------------------'            `--------------------'   `-----'
   */
  [_MEDIA] = LAYOUT(
- 	KC_NO,      KC_NO,		KC_NO,		KC_NO,	    KC_NO,		RGB_TOG,	RGB_MOD,	RGB_HUI,	RGB_SAI,	RGB_VAI,
+ 	QK_BOOT,    KC_NO,		KC_NO,		KC_NO,	    KC_NO,		RGB_TOG,	RGB_MOD,	RGB_HUI,	RGB_SAI,	RGB_VAI,
  	KC_LGUI,	KC_LALT,	KC_LCTL,	KC_LSFT,	KC_NO,		KC_NO,		KC_MPRV,    KC_VOLD,	KC_VOLU,	KC_MNXT,
  	KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,	    KC_NO,		KC_NO,		KC_NO,
  	KC_NO,		KC_NO,      KC_NO,      KC_NO,		KC_NO,	    KC_NO,		KC_MSTP,	KC_MPLY,	KC_MUTE,    KC_NO
@@ -144,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *          `-----'   `--------------------'            `--------------------'   `-----'
   */
  [_NUM] = LAYOUT(
- 	KC_LBRC,	KC_7,	KC_8,	KC_9,	    KC_RBRC,		KC_NO,	    KC_NO,	    KC_NO,		KC_NO,		KC_NO,
+ 	KC_LBRC,	KC_7,	KC_8,	KC_9,	    KC_RBRC,		KC_NO,	    KC_NO,	    KC_NO,		KC_NO,		QK_BOOT,
  	KC_SCLN,	KC_4,	KC_5,	KC_6,	    KC_EQUAL,		KC_NO,		KC_RSFT,    KC_RCTL,	KC_RALT,	KC_RGUI,
  	KC_GRAVE,	KC_1,	KC_2,	KC_3,		KC_BSLS,		KC_NO,		KC_NO,	    KC_NO,		KC_NO,		KC_NO,
  	KC_NO,		KC_DOT, KC_0,   KC_MINUS,	KC_NO,	        KC_NO,		KC_NO,	    KC_NO,	    KC_NO,      KC_NO
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *          `-----'   `--------------------'            `--------------------'   `-----'
   */
  [_SYM] = LAYOUT(
- 	KC_LCBR,	KC_AMPR,	KC_ASTR,	KC_LPRN,	KC_RCBR,	KC_NO,	    KC_NO,	    KC_NO,		KC_NO,		KC_NO,
+ 	KC_LCBR,	KC_AMPR,	KC_ASTR,	KC_LPRN,	KC_RCBR,	KC_NO,	    KC_NO,	    KC_NO,		KC_NO,		QK_BOOT,
  	KC_COLN,	KC_DLR,	    KC_PERC,	KC_CIRC,	KC_PLUS,	KC_NO,		KC_RSFT,    KC_RCTL,	KC_RALT,	KC_RGUI,
  	KC_TILD,	KC_EXLM,	KC_AT,		KC_HASH,	KC_PIPE,	KC_NO,		KC_NO,	    KC_NO,		KC_NO,		KC_NO,
  	KC_NO,      KC_LPRN,    KC_RPRN,    KC_UNDS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO
@@ -184,7 +184,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *          `-----'   `--------------------'            `--------------------'   `-----'
   */
  [_FUNC] = LAYOUT(
- 	KC_F12,		KC_F7,		KC_F8,		KC_F9,	    KC_PSCR,	KC_NO,	    KC_NO,	    KC_NO,		KC_NO,		KC_NO,
+ 	KC_F12,		KC_F7,		KC_F8,		KC_F9,	    KC_PSCR,	KC_NO,	    KC_NO,	    KC_NO,		KC_NO,		QK_BOOT,
  	KC_F11,	    KC_F4,	    KC_F5,	    KC_F6,	    KC_SCRL,	KC_NO,		KC_RSFT,    KC_RCTL,	KC_RALT,	KC_RGUI,
  	KC_F10,		KC_F1,		KC_F2,		KC_F3,		KC_PAUS,	KC_NO,		KC_NO,	    KC_NO,		KC_NO,		KC_NO,
  	KC_NO,		KC_APP,     KC_SPC,     KC_TAB,		KC_NO,	    KC_NO,		KC_NO,	    KC_NO,	    KC_NO,      KC_NO
